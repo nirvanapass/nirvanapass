b1d7ae11f45c97f774695778787cdcc52b186a57
Pratyush Nalam <pratyushnalam@outlook.com>
2016-02-06 15:22:14 -0500

    Argon2 works!

--- prototype/src/main/java/com/nirvanapass/Main.java
+++ prototype/src/main/java/com/nirvanapass/Main.java
@@ -1,17 +1,19 @@
 package com.nirvanapass;
 
-import com.lambdaworks.crypto.SCrypt;
-
-import java.security.GeneralSecurityException;
-import java.util.Arrays;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            System.out.println(Arrays.toString(SCrypt.scrypt("hello world".getBytes(), "pratyush nalam".getBytes(), 16, 8, 1, 64)));
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            System.out.println(Arrays.toString(SCrypt.scrypt("hello world".getBytes(), "pratyush nalam".getBytes(),
+// 16, 8, 1, 64)));
+        Argon2 argon2 = Argon2Factory.create();
+        String hash = argon2.hash(2, 65536, 1, "password");
+        System.out.println(hash);
+//        } catch (GeneralSecurityException e) {
+//            e.printStackTrace();
+//        }
     }
 }
