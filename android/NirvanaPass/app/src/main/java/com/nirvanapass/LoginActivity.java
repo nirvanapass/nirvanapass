0986a01f33850231fda0ba40e6f099298a6a3442
Pratyush Nalam <pratyushnalam@outlook.com>
2016-08-11 22:54:40 -0400

    Refactoring of the header. New blank password activity

--- android/NirvanaPass/app/src/main/java/com/nirvanapass/LoginActivity.java
+++ android/NirvanaPass/app/src/main/java/com/nirvanapass/LoginActivity.java
@@ -11,7 +11,6 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.lambdaworks.crypto.SCrypt;
 
