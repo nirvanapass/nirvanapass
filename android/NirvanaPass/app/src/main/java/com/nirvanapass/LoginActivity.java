2346e882f68355b512cd730c1f5ac9d1f6fdb859
Pratyush Nalam <pratyushnalam@outlook.com>
2016-08-16 22:12:41 -0400

    Added tabs to the activity after logging in

--- android/NirvanaPass/app/src/main/java/com/nirvanapass/LoginActivity.java
+++ android/NirvanaPass/app/src/main/java/com/nirvanapass/LoginActivity.java
@@ -1,21 +1,14 @@
 package com.nirvanapass;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import com.lambdaworks.crypto.SCrypt;
-
-import java.security.GeneralSecurityException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -27,6 +20,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity_layout);
+
         loginNameEditText = (EditText) findViewById(R.id.loginNameEditText);
         loginPasswordEditText = (EditText) findViewById(R.id.loginPasswordEditText);
         loginSubmitButton = (Button) findViewById(R.id.loginSubmitButton);
@@ -44,18 +38,18 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onClickLoginSubmitButton(View view) {
-        try {
-            String hash = Base64.encodeToString(SCrypt.scrypt(loginPasswordEditText.getText().toString().getBytes(), loginNameEditText.getText().toString().getBytes(), 16384, 16, 2, 128), Base64.DEFAULT);
-            SharedPreferences userSession = getSharedPreferences("userSession", Context.MODE_PRIVATE);
-            SharedPreferences.Editor userSessionEditor = userSession.edit();
-            userSessionEditor.putString("name", loginNameEditText.getText().toString());
-            userSessionEditor.putString("hash", hash);
-            userSessionEditor.apply();
+//        try {
+//            String hash = Base64.encodeToString(SCrypt.scrypt(loginPasswordEditText.getText().toString().getBytes(), loginNameEditText.getText().toString().getBytes(), 16384, 16, 2, 128), Base64.DEFAULT);
+//            SharedPreferences userSession = getSharedPreferences("userSession", Context.MODE_PRIVATE);
+//            SharedPreferences.Editor userSessionEditor = userSession.edit();
+//            userSessionEditor.putString("name", loginNameEditText.getText().toString());
+//            userSessionEditor.putString("hash", hash);
+//            userSessionEditor.apply();
 
-            Intent intent = new Intent(this, PasswordActivity.class);
-            startActivity(intent);
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        }
+        Intent intent = new Intent(this, PasswordActivity.class);
+        startActivity(intent);
+//        } catch (GeneralSecurityException e) {
+//            e.printStackTrace();
+//        }
     }
 }
--- android/NirvanaPass/app/src/main/java/com/nirvanapass/PasswordActivity.java
+++ android/NirvanaPass/app/src/main/java/com/nirvanapass/PasswordActivity.java
@@ -1,7 +1,9 @@
 package com.nirvanapass;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
 
 public class PasswordActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class PasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.password_activity_layout);
+
+        TabHost passwordTabHost = (TabHost) findViewById(R.id.passwordTabHost);
+        passwordTabHost.setup();
+
+        TabHost.TabSpec spec = passwordTabHost.newTabSpec("Generate");
+        spec.setContent(R.id.passwordHomeLayout);
+        spec.setIndicator("", ContextCompat.getDrawable(this, R.drawable.password));
+        passwordTabHost.addTab(spec);
+
+        spec = passwordTabHost.newTabSpec("Me");
+        spec.setContent(R.id.profileLayout);
+        spec.setIndicator("", ContextCompat.getDrawable(this, R.drawable.user));
+        passwordTabHost.addTab(spec);
+
+        spec = passwordTabHost.newTabSpec("Help");
+        spec.setContent(R.id.helpLayout);
+        spec.setIndicator("", ContextCompat.getDrawable(this, R.drawable.help));
+        passwordTabHost.addTab(spec);
     }
 }
